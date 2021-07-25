458a3d0e28290512ee4ecc24c43112e6c28f8662
Lukáš Matuška <lukynmatuska@gmail.com>
2021-07-25 13:07:05 +0200

    Landing!

--- src/components/Footers/Footer.svelte
+++ src/components/Footers/Footer.svelte
@@ -20,127 +20,47 @@
       <polygon
         class="text-blueGray-200 fill-current"
         points="2560 0 2560 100 0 100"
-      ></polygon>
+      />
     </svg>
   </div>
   <div class="container mx-auto px-4">
     <div class="flex flex-wrap text-center lg:text-left">
       <div class="w-full lg:w-6/12 px-4">
-        <h4 class="text-3xl font-semibold">Let's keep in touch!</h4>
+        <h4 class="text-3xl font-semibold">Zůstaňte v kontaktu!</h4>
         <h5 class="text-lg mt-0 mb-2 text-blueGray-600">
-          Find us on any of these platforms, we respond 1-2 business days.
+          Sledujte nás na všech sociálních sítích, ať Vám nic neuteče.
         </h5>
         <div class="mt-6 lg:mb-0 mb-6">
-          <button
-            class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-            type="button"
+          <a
+            href="https://www.facebook.com/groups/153340168045479"
+            target="_blank"
           >
-            <i class="fab fa-twitter"></i>
-          </button>
-          <button
-            class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-            type="button"
-          >
-            <i class="fab fa-facebook-square"></i>
-          </button>
-          <button
-            class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-            type="button"
-          >
-            <i class="fab fa-dribbble"></i>
-          </button>
-          <button
-            class="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-            type="button"
-          >
-            <i class="fab fa-github"></i>
-          </button>
-        </div>
-      </div>
-      <div class="w-full lg:w-6/12 px-4">
-        <div class="flex flex-wrap items-top mb-6">
-          <div class="w-full lg:w-4/12 px-4 ml-auto">
-            <span
-              class="block uppercase text-blueGray-500 text-sm font-semibold mb-2"
+            <button
+              class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+              type="button"
             >
-              Useful Links
-            </span>
-            <ul class="list-unstyled">
-              <li>
-                <a
-                  class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                  href="https://www.creative-tim.com/presentation?ref=ns-footer"
-                >
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                  href="https://blog.creative-tim.com?ref=ns-footer"
-                >
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                  href="https://www.github.com/creativetimofficial?ref=ns-footer"
-                >
-                  Github
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                  href="https://www.creative-tim.com/bootstrap-themes/free?ref=ns-footer"
-                >
-                  Free Products
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="w-full lg:w-4/12 px-4">
-            <span
-              class="block uppercase text-blueGray-500 text-sm font-semibold mb-2"
+              <i class="fab fa-facebook-square" />
+            </button>
+          </a>
+          <a
+            href="https://www.instagram.com/explore/tags/nasevcb/"
+            target="_blank"
+          >
+            <button
+              class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+              type="button"
             >
-              Other Resources
-            </span>
-            <ul class="list-unstyled">
-              <li>
-                <a
-                  class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                  href="https://github.com/creativetimofficial/notus-svelte/blob/main/LICENSE.md?ref=ns-footer"
-                >
-                  MIT License
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                  href="https://creative-tim.com/terms?ref=ns-footer"
-                >
-                  Terms & Conditions
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                  href="https://creative-tim.com/privacy?ref=ns-footer"
-                >
-                  Privacy Policy
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                  href="https://creative-tim.com/contact-us?ref=ns-footer"
-                >
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
+              <i class="fab fa-instagram" />
+            </button>
+          </a>
+          <a href="https://github.com/vvcb-cz" target="_blank">
+            <button
+              class="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+              type="button"
+            >
+              <i class="fab fa-github" />
+            </button>
+          </a>
         </div>
       </div>
     </div>
@@ -148,14 +68,22 @@
     <div class="flex flex-wrap items-center md:justify-between justify-center">
       <div class="w-full md:w-4/12 px-4 mx-auto text-center">
         <div class="text-sm text-blueGray-500 font-semibold py-1">
-          Copyright © {date} Notus Svelte by
+          Copyright © {date}
           <a
-            href="https://www.creative-tim.com?ref=ns-footer"
+            href="https://lukasmatuska.cz/"
+            target="_blank"
             class="text-blueGray-500 hover:text-blueGray-800"
           >
-            Creative Tim
+            Lukáš Matuška
           </a>
-          .
+          &
+          <a
+            href="https://mbucek.cz/"
+            target="_blank"
+            class="text-blueGray-500 hover:text-blueGray-800"
+          >
+            Matěj Bucek
+          </a>
         </div>
       </div>
     </div>
--- src/components/Navbars/AuthNavbar.svelte
+++ src/components/Navbars/AuthNavbar.svelte
@@ -2,7 +2,7 @@
   import { link } from "svelte-routing";
 
   // core components
-  import PagesDropdown from "../Dropdowns/PagesDropdown.svelte";
+  import ResultsDropdown from "../Dropdowns/ResultsDropdown.svelte";
 
   let navbarOpen = false;
 
@@ -25,83 +25,64 @@
         class="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
         href="/"
       >
-        Notus Svelte
+        VVCB
       </a>
       <button
         class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
         type="button"
-        on:click="{setNavbarOpen}"
+        on:click={setNavbarOpen}
       >
-        <i class="text-white fas fa-bars"></i>
+        <i class="text-white fas fa-bars" />
       </button>
     </div>
     <div
-      class="lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none rounded shadow-lg {navbarOpen ? 'block':'hidden'}"
+      class="lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none rounded shadow-lg {navbarOpen
+        ? 'block'
+        : 'hidden'}"
       id="example-navbar-warning"
     >
-      <ul class="flex flex-col lg:flex-row list-none mr-auto">
-        <li class="flex items-center">
-          <a
-            class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-            href="https://www.creative-tim.com/learning-lab/tailwind/svelte/overview/notus?ref=ns-auth-navbar"
-          >
-            <i
-              class="lg:text-blueGray-200 text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2"
-            />
-            Docs
-          </a>
-        </li>
-      </ul>
+      <!-- <ul class="flex flex-col lg:flex-row list-none mr-auto" /> -->
       <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
         <li class="flex items-center">
-          <PagesDropdown />
+          <ResultsDropdown />
         </li>
         <li class="flex items-center">
           <a
             class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-            href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-svelte%2F%23%2F"
+            href="https://www.facebook.com/groups/153340168045479"
             target="_blank"
           >
             <i
               class="lg:text-blueGray-200 text-blueGray-400 fab fa-facebook text-lg leading-lg"
             />
-            <span class="lg:hidden inline-block ml-2">Share</span>
+            <span class="lg:hidden inline-block ml-2">FB Skupina</span>
           </a>
         </li>
 
         <li class="flex items-center">
           <a
             class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-            href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-svelte%2F&text=Start%20your%20development%20with%20a%20Free%20Tailwind%20CSS%20and%20Svelte%20UI%20Kit%20and%20Admin.%20Let%20Notus%20Svelte%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level."
+            href="https://www.instagram.com/explore/tags/nasevcb/"
             target="_blank"
           >
             <i
-              class="lg:text-blueGray-200 text-blueGray-400 fab fa-twitter text-lg leading-lg"
+              class="lg:text-blueGray-200 text-blueGray-400 fab fa-instagram text-lg leading-lg"
             />
-            <span class="lg:hidden inline-block ml-2">Tweet</span>
+            <span class="lg:hidden inline-block ml-2">Instagram</span>
           </a>
         </li>
 
         <li class="flex items-center">
           <a
             class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-            href="https://github.com/creativetimofficial/notus-svelte?ref=ns-auth-navbar"
+            href="https://github.com/vvcb-cz"
             target="_blank"
           >
             <i
               class="lg:text-blueGray-200 text-blueGray-400 fab fa-github text-lg leading-lg"
             />
-            <span class="lg:hidden inline-block ml-2">Star</span>
+            <span class="lg:hidden inline-block ml-2">GitHub</span>
           </a>
-        </li>
-
-        <li class="flex items-center">
-          <button
-            class="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
-            type="button"
-          >
-            <i class="fas fa-arrow-alt-circle-down"></i> Download
-          </button>
         </li>
       </ul>
     </div>
