5ce3907c7766e62eca846390afa1c2689e4ab788
Lukáš Matuška <lukynmatuska@gmail.com>
2021-07-25 20:09:12 +0200

    Improve Login

--- src/views/auth/Login.svelte
+++ src/views/auth/Login.svelte
@@ -13,34 +13,7 @@
       <div
         class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
       >
-        <div class="rounded-t mb-0 px-6 py-6">
-          <div class="text-center mb-3">
-            <h6 class="text-blueGray-500 text-sm font-bold">
-              Sign in with
-            </h6>
-          </div>
-          <div class="btn-wrapper text-center">
-            <button
-              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-              type="button"
-            >
-              <img alt="..." class="w-5 mr-1" src="{github}" />
-              Github
-            </button>
-            <button
-              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-              type="button"
-            >
-              <img alt="..." class="w-5 mr-1" src="{google}" />
-              Google
-            </button>
-          </div>
-          <hr class="mt-6 border-b-1 border-blueGray-300" />
-        </div>
-        <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <div class="text-blueGray-400 text-center mb-3 font-bold">
-            <small>Or sign in with credentials</small>
-          </div>
+        <div class="flex-auto px-4 lg:px-10 py-6 mb-0">
           <form>
             <div class="relative w-full mb-3">
               <label
@@ -62,26 +35,14 @@
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 for="grid-password"
               >
-                Password
+                Heslo
               </label>
               <input
                 id="grid-password"
                 type="password"
                 class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                placeholder="Password"
+                placeholder="Heslo"
               />
-            </div>
-            <div>
-              <label class="inline-flex items-center cursor-pointer">
-                <input
-                  id="customCheckLogin"
-                  type="checkbox"
-                  class="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                />
-                <span class="ml-2 text-sm font-semibold text-blueGray-600">
-                  Remember me
-                </span>
-              </label>
             </div>
 
             <div class="text-center mt-6">
@@ -89,24 +50,47 @@
                 class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                 type="button"
               >
-                Sign In
+                Přihlásit
               </button>
             </div>
           </form>
         </div>
+        <div class="rounded-t mt-0 px-6 pb-6">
+          <hr class="mb-6 border-b-1 border-blueGray-300" />
+          <div class="btn-wrapper text-center">
+            <button
+              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+              type="button"
+            >
+              <img alt="..." class="w-5 mr-1" src={github} />
+              Github
+            </button>
+            <button
+              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+              type="button"
+            >
+              <img alt="..." class="w-5 mr-1" src={google} />
+              Google
+            </button>
+          </div>
+        </div>
       </div>
-      <div class="flex flex-wrap mt-6 relative">
+      <!-- <div class="flex flex-wrap mt-6 relative">
         <div class="w-1/2">
-          <a href="#pablo" on:click={(e) => e.preventDefault()} class="text-blueGray-200">
-            <small>Forgot password?</small>
+          <a
+            href="#pablo"
+            on:click={(e) => e.preventDefault()}
+            class="text-blueGray-200"
+          >
+            <small>Zapomenuté heslo</small>
           </a>
         </div>
         <div class="w-1/2 text-right">
           <a use:link href="/auth/register" class="text-blueGray-200">
-            <small>Create new account</small>
+            <small>Registrace</small>
           </a>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </div>
