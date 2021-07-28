c4dfec37bc7043821ab347162a3f140d675581dd
Lukáš Matuška <lukynmatuska@gmail.com>
2021-07-28 17:55:46 +0200

    Improve Index

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -3,6 +3,7 @@
     import AuthNavbar from "../components/Navbars/AuthNavbar.svelte";
     import Footer from "../components/Footers/Footer.svelte";
     import { Link } from "svelte-routing";
+    import CardTeamsTable from "../components/Cards/CardTeamsTable.svelte";
 
     export let location;
 </script>
@@ -67,5 +68,23 @@
             </div>
         </div>
     </main>
+
+    <section class="pb-20 bg-blueGray-200 -mt-24">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-wrap">
+                <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+                    <CardTeamsTable />
+                </div>
+
+                <div class="w-full md:w-4/12 px-4 text-center">
+                    <CardTeamsTable title="Následující závody" />
+                </div>
+
+                <div class="pt-6 w-full md:w-4/12 px-4 text-center">
+                    <CardTeamsTable title="Nejrychlejší sestřiky" />
+                </div>
+            </div>
+        </div>
+    </section>
     <Footer />
 </div>
