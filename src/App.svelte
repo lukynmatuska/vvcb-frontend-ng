5abc6072000663441dc7ff7049ce40f71e347feb
Lukáš Matuška <lukynmatuska@gmail.com>
2021-08-03 13:20:28 +0200

    Remove old components from App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,28 +6,24 @@
   import Admin from "./layouts/Admin.svelte";
   // Auth Layout
   import AuthLayout from "./layouts/Auth.svelte";
-  import IndexOld from "./views/Index-old.svelte";
 
   // No Layout Pages
   import Index from "./views/Index.svelte";
-  import Landing from "./views/Landing.svelte";
-  import Profile from "./views/Profile.svelte";
   import Results from "./views/Results.svelte";
   import Monitors from "./views/monitors/Monitors.svelte";
 
-  import {authStore} from "./components/Auth/Stores";
-  import {Auth} from "./components/Auth/Auth.service";
+  import { authStore } from "./components/Auth/Stores";
+  import { Auth } from "./components/Auth/Auth.service";
 
   let auth = new Auth({
-				realm: "vvcb",
-				"auth-server-url": "https://auth.vvcb.cz/auth",
-				"ssl-required": "external",
-				resource: "frontend",
-				clientId: "frontend",
-				"public-client": true,
-				"confidential-port": 0,
-			});
-
+    realm: "vvcb",
+    "auth-server-url": "https://auth.vvcb.cz/auth",
+    "ssl-required": "external",
+    resource: "frontend",
+    clientId: "frontend",
+    "public-client": true,
+    "confidential-port": 0,
+  });
 
   authStore.set(auth);
 
@@ -56,9 +52,6 @@
   <!-- auth layout -->
   <Route path="auth/*auth" component={AuthLayout} />
   <!-- no layout pages -->
-  <Route path="landing" component={Landing} />
-  <Route path="profile" component={Profile} />
-  <Route path="index" component={IndexOld} />
   <Route path="results" component={Results} />
   <Route path="monitors" component={Monitors} />
   <Route path="/" component={Index} />
