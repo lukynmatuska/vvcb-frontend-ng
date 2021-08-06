6fd35e626e2033ecd062f1355af4ff5825eaa159
Lukáš Matuška <lukynmatuska@gmail.com>
2021-08-06 09:58:09 +0200

    Update enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,15 @@ export const environment = {
   production: true,
   backend: {
     api: "https://api.vvcb.cz"
+  },
+  keycloak: {
+    issuer: 'https://auth.vvcb.cz/auth/realms/vvcb',
+    redirectUri: 'http://localhost:4200/',
+    clientId: 'frontend',
+    responseType: 'code',
+    scope: 'openid profile email',
+    requireHttps: true,
+    showDebugInformation: true,
+    disableAtHashCheck: true
   }
 };
