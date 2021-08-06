4a195c43a91120d45b577c86de47a4ae285b42a2
Matěj Bucek <admin@mbucek.cz>
2021-08-06 14:03:45 +0200

    VVCB-Frontend: ResultActionBar fix.

--- src/app/components/action-bars/result-action-bar/result-action-bar.component.ts
+++ src/app/components/action-bars/result-action-bar/result-action-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ResultTemplateService } from 'src/app/services/result-template.service';
 import { ResultService } from 'src/app/services/result.service';
 import { Result } from 'src/app/types/result';
 
@@ -11,13 +12,13 @@ export class ResultActionBarComponent implements OnInit {
 
   // @Input("result") result!: Result;
 
-  constructor(private readonly resultService: ResultService) { }
+  constructor(private readonly resultService: ResultTemplateService) { }
 
   ngOnInit(): void {
   }
 
   public add() {
     console.log("add");
-    // this.resultService.registerResult(this.result);
+    this.resultService.registerResultTemplate({});
   }
 }
