0b8ed33b6fba92ab7f9e3e13ef19767667582de8
Lukáš Matuška <lukynmatuska@gmail.com>
2021-08-11 12:03:35 +0200

    Delete unsusable block of code - result-action-bar

--- src/app/components/action-bars/result-action-bar/result-action-bar.component.ts
+++ src/app/components/action-bars/result-action-bar/result-action-bar.component.ts
@@ -1,7 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ResultTemplateService } from 'src/app/services/result-template.service';
-import { ResultService } from 'src/app/services/result.service';
-import { Result } from 'src/app/types/result';
 
 @Component({
   selector: 'app-result-action-bar',
@@ -10,15 +8,12 @@ import { Result } from 'src/app/types/result';
 })
 export class ResultActionBarComponent implements OnInit {
 
-  // @Input("result") result!: Result;
-
   constructor(private readonly resultService: ResultTemplateService) { }
 
   ngOnInit(): void {
   }
 
   public add() {
-    console.log("add");
     this.resultService.registerResultTemplate({});
   }
 }
