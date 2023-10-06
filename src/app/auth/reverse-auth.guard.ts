42f6b85754197b60200e412a49384f9221cddbe8
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2023-10-07 01:10:30 +0200

    Update angular

--- src/app/auth/reverse-auth.guard.ts
+++ src/app/auth/reverse-auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ReverseAuthGuard implements CanActivate {
+export class ReverseAuthGuard  {
   constructor(private readonly authService: AuthService, private readonly router: Router){}
 
   canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
